ee0ab575ea0f5a2a76f2f68c334ff89c8cdf2e87
sleepbear <sleepbear669@gmail.com>
2016-11-08 23:33:12 +0900

    Apply bable es2015

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,19 @@ module.exports = {
     },
     output: {
         filename: "./src/[name]/bundle.js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js?$/,
+                exclude: /node_modules/,
+                loader: 'babel', // 'babel-loader' is also a legal name to reference
+                query: {
+                    cacheDirectory: true,
+                    presets: ['es2015', 'stage-2']
+                }
+            }
+        ]
     }
+
 };
